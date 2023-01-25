dede436fc8b77882fb498f5ec44ad1f17fbf5f06
Mike <grabbou@gmail.com>
2023-01-25 18:17:34 -0500

    chore: add todo

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@
 require('@uniswap/eslint-config/load')
 
 module.exports = {
+  // @todo create `uniswap/eslint-config/next`
   extends: ['@uniswap/eslint-config/react', 'plugin:@next/next/core-web-vitals', 'plugin:@next/next/recommended'],
   rules: {
     'import/no-unused-modules': 0,
