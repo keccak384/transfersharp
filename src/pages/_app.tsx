b6f3b2f7123b09fa90a2f757d404cb097616427c
Mike Grabowski <grabbou@gmail.com>
2023-01-26 08:49:03 -0500

    feat: clean up css and remove default Next.js files (#5)
    * feat: clean up css
    
    * that was unecessary
    
    * chore: bring back original name

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
-import '@/styles/globals.css'
-
+import { styled } from '@stitches/react'
 import type { AppProps } from 'next/app'
 
+const AppWrapper = styled('div', {
+  display: 'flex',
+  width: '100vw',
+  minHeight: '100vh',
+  overflow: 'scroll',
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AppWrapper>
+      <Component {...pageProps} />
+    </AppWrapper>
+  )
 }
--- stitches.config.ts
+++ stitches.config.ts
@@ -1,10 +1,10 @@
-import { blue, gray, green, red } from "@radix-ui/colors";
-import { createStitches, globalCss } from "@stitches/react";
+import { blue, gray, green, red } from '@radix-ui/colors'
+import { createStitches, globalCss } from '@stitches/react'
 
 export const { styled, getCssText } = createStitches({
   theme: {
     fonts: {
-      system: "system-ui",
+      system: 'system-ui',
     },
     colors: {
       ...gray,
@@ -13,32 +13,40 @@ export const { styled, getCssText } = createStitches({
       ...green,
     },
     space: {
-      1: "4px",
-      2: "8px",
-      3: "12px",
-      4: "16px",
+      1: '4px',
+      2: '8px',
+      3: '12px',
+      4: '16px',
     },
     fontSizes: {
-      1: "14px",
-      2: "16px",
-      3: "18px",
+      1: '14px',
+      2: '16px',
+      3: '18px',
     },
   },
-});
+})
 
 const GlobalStyles = globalCss({
-  "*": { margin: 0, padding: 0 },
+  '*': { margin: 0, padding: 0, boxSizing: 'border-box' },
   input: {
-    appearance: "textfield",
+    appearance: 'textfield',
+  },
+  html: {
+    maxWidth: '100vw',
+    overflowX: 'hidden',
   },
   body: {
-    //we can call the color token values with the
-    //$ prefix in a string
-    background: "$background",
-    color: "$gray11",
-    fontSize: "$2",
-    fontFamily: "$system",
+    maxWidth: '100vw',
+    overflowX: 'hidden',
+    background: '$background',
+    color: '$gray11',
+    fontSize: '$2',
+    fontFamily: '$system',
   },
-});
+  a: {
+    color: 'inherit',
+    textDecoration: 'none',
+  },
+})
 
-GlobalStyles();
+GlobalStyles()
