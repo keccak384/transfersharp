f1a27fff4d5f3559ccfc438d886c9d6ad6f992a2
Vignesh Mohankumar <me@vig.xyz>
2023-01-26 18:05:46 -0500

    swap

--- src/components/SwapForm.tsx
+++ src/components/SwapForm.tsx
@@ -1,6 +1,6 @@
 import * as Label from '@radix-ui/react-label'
 import { CurrencyAmount, Token, TradeType } from '@uniswap/sdk-core'
-import { Pool, FeeAmount, Route } from '@uniswap/v3-sdk'
+import { Pool, FeeAmount, Route, SwapRouter, SwapOptions, Trade } from '@uniswap/v3-sdk'
 // import { Trade } from '@uniswap/router-sdk'
 import { useAtom } from 'jotai'
 import Image from 'next/image'
@@ -26,6 +26,8 @@ const EUROC_MAINNET = new Token(
   'EURO//C'
 )
 
+const SWAP_ROUTER_ADDRESS = '0xE592427A0AEce92De3Edee1F18E0157C05861564'
+
 function fetchQuote(amount: number) {
   return fetch(
     `https://jnru9d0d29.execute-api.us-east-1.amazonaws.com/prod/quote?tokenInAddress=0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48&tokenInChainId=1&tokenOutAddress=0x1aBaEA1f7C830bD89Acc67eC4af516284b1bC33c&tokenOutChainId=1&amount=${amount}&type=exactIn`
@@ -50,12 +52,31 @@ export default function SwapForm() {
       const pool = new Pool(USDC_MAINNET, EUROC_MAINNET, FeeAmount.LOW, json.route[0][0]?.sqrtRatioX96, json.route[0][0]?.liquidity, parseInt(json.route[0][0]?.tickCurrent)) 
 
       const route = new Route([pool], USDC_MAINNET, EUROC_MAINNET)
-      const amount = CurrencyAmount.fromRawAmount(USDC_MAINNET, inputValue)
-      console.log(amount)
-      const trade = await Trade.fromRoute(route, amount, TradeType.EXACT_INPUT)
+      const inputAmount = CurrencyAmount.fromRawAmount(USDC_MAINNET, inputValue)
+      const outputAmount = CurrencyAmount.fromRawAmount(EUROC_MAINNET, outputValue)
 
-      console.log(trade)
-      setOutputValue(json.quote)
+      const uncheckedTrade = Trade.createUncheckedTrade({
+        route,
+        inputAmount,
+        outputAmount,
+        tradeType: TradeType.EXACT_INPUT,
+      })
+      console.log(uncheckedTrade)
+
+      const options: SwapOptions = {
+        slippageTolerance: new Percent(500, 10000), // 50 bips, or 0.50%
+        deadline: Math.floor(Date.now() / 1000) + 60 * 20, // 20 minutes from the current Unix time
+        recipient: '0x703491E54970DC622c3b77D49B6727d5b69Eb45C',
+      }
+
+      const methodParameters = SwapRouter.swapCallParameters([uncheckedTrade], options)
+
+      const tx = {
+        data: methodParameters.calldata,
+        to: SWAP_ROUTER_ADDRESS,
+        value: methodParameters.value,
+        from: walletAddress,
+      }
     })
   }, [inputValue, setOutputValue])
 
