91830e2dd65fe6c8013cac2b1f5f9fc1a0438a14
Mike Grabowski <grabbou@gmail.com>
2023-01-26 16:05:18 -0500

    add twilio (#23)

--- src/env.tsx
+++ src/env.tsx
@@ -1,6 +1,12 @@
+const IS_SERVER = typeof window === 'undefined'
+
 // @todo schema validation
 if (!process.env.NEXT_PUBLIC_MAGIC_PUB_KEY) {
   throw new Error('NEXT_PUBLIC_MAGIC_PUB_KEY is required')
+}
+
+if (IS_SERVER && (!process.env.TWILIO_ACCOUNT_SID || !process.env.TWILIO_TOKEN || !process.env.TWILIO_PHONE)) {
+  throw new Error('TWILIO_ACCOUNT_SID, TWILIO_TOKEN and TWILIO_PHONE are required')
 }
 
 /**
@@ -8,3 +14,7 @@ if (!process.env.NEXT_PUBLIC_MAGIC_PUB_KEY) {
  * You can set them in `.env.local`
  */
 export const MAGIC_PUBLIC_KEY = process.env.NEXT_PUBLIC_MAGIC_PUB_KEY
+
+export const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID
+export const TWILIO_TOKEN = process.env.TWILIO_TOKEN
+export const TWILIO_PHONE = process.env.TWILIO_PHONE
