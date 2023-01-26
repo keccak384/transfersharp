cfddd9547129c59faa250236c6459fee884d37db
Mike Grabowski <grabbou@gmail.com>
2023-01-26 16:45:02 -0500

    feat: notify sender/receiver  (#25)
    * initial
    
    * complete

--- src/pages/send/[transactionId].tsx
+++ src/pages/send/[transactionId].tsx
@@ -43,7 +43,7 @@ export async function getServerSideProps({ params: { transactionId } }: { params
 }
 
 function SendTransaction({ transaction }: { transaction: Transaction }) {
-  const didReceiverAccept = false
+  const didReceiverAccept = !!transaction.toWallet
   const ButtonComponent = didReceiverAccept ? Button : PendingButton
 
   const isLoggedIn = useAtomValue(isLoggedInAtom)
