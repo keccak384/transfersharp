1b64f9930d8f3e46e31b767844d7117cddfd0b2b
Mike <grabbou@gmail.com>
2023-01-26 10:11:57 -0500

    oops

--- src/db/transactions.ts
+++ src/db/transactions.ts
@@ -1,4 +1,4 @@
-import * as fs from 'fs'
+import * as fs from 'fs/promises'
 
 export type Transaction = {
   id: string
@@ -9,13 +9,13 @@ export type Transaction = {
 }
 
 export const addTransaction = async (transaction: Transaction) => {
-  fs.writeFileSync(`/tmp/${transaction.id}.json`, JSON.stringify(transaction))
+  await fs.writeFile(`/tmp/${transaction.id}.json`, JSON.stringify(transaction))
   return transaction
 }
 
 export const getTransactionById = async (id: string) => {
   try {
-    return JSON.parse(fs.readFileSync(`/tmp/${id}.json`, 'utf8'))
+    return JSON.parse(await fs.readFile(`/tmp/${id}.json`, 'utf8'))
   } catch (e) {
     console.log(`Error while reading transactions: ${e}`)
     return null
