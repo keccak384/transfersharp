05d8b9f6c043c28aa8ef34c278915d460345b2df
Mike Grabowski <grabbou@gmail.com>
2023-01-27 09:10:15 -0500

    feat: use s3 (#35)

--- src/db/transactions.ts
+++ src/db/transactions.ts
@@ -1,4 +1,14 @@
-import * as fs from 'fs/promises'
+import { GetObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
+
+import { AWS_ACCESS_KEY, AWS_BUCKET_NAME, AWS_REGION, AWS_SECRET_KEY } from '@/env'
+
+const s3Client = new S3Client({
+  region: AWS_REGION,
+  credentials: {
+    accessKeyId: AWS_ACCESS_KEY,
+    secretAccessKey: AWS_SECRET_KEY,
+  },
+})
 
 export type Transaction = {
   id: string
@@ -14,10 +24,29 @@ export type Transaction = {
 }
 
 export const getTransactionById = async (id: string): Promise<Transaction> => {
-  return JSON.parse(await fs.readFile(`/tmp/${id}.json`, 'utf8'))
+  const response = await s3Client.send(
+    new GetObjectCommand({
+      Bucket: AWS_BUCKET_NAME,
+      Key: id,
+    })
+  )
+
+  const body = await response.Body?.transformToString()
+
+  if (!body) {
+    throw new Error('Invalid transaction')
+  }
+
+  return JSON.parse(body)
 }
 
 export const saveTransaction = async (transaction: Transaction) => {
-  await fs.writeFile(`/tmp/${transaction.id}.json`, JSON.stringify(transaction))
+  await s3Client.send(
+    new PutObjectCommand({
+      Bucket: AWS_BUCKET_NAME,
+      Key: transaction.id,
+      Body: JSON.stringify(transaction),
+    })
+  )
   return transaction
 }
