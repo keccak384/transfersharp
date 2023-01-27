3e2ba2b8b9073bca8596b0338a3c09de4e3fb483
Mike <grabbou@gmail.com>
2023-01-27 10:04:23 -0500

    chore: update var names

--- src/env.tsx
+++ src/env.tsx
@@ -9,8 +9,8 @@ if (IS_SERVER && (!process.env.TWILIO_ACCOUNT_SID || !process.env.TWILIO_TOKEN |
   throw new Error('TWILIO_ACCOUNT_SID, TWILIO_TOKEN and TWILIO_PHONE are required')
 }
 
-if (IS_SERVER && (!process.env.AWS_ACCESS_KEY || !process.env.AWS_SECRET_KEY || !process.env.AWS_REGION)) {
-  throw new Error('AWS_ACCESS_KEY, AWS_SECRET_KEY and AWS_REGION are required')
+if (IS_SERVER && (!process.env.FX_AWS_ACCESS_KEY || !process.env.FX_AWS_SECRET_KEY || !process.env.FX_AWS_REGION)) {
+  throw new Error('FX_AWS_ACCESS_KEY, FX_AWS_SECRET_KEY and FX_AWS_REGION are required')
 }
 
 /**
@@ -23,7 +23,7 @@ export const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID as string
 export const TWILIO_TOKEN = process.env.TWILIO_TOKEN as string
 export const TWILIO_PHONE = process.env.TWILIO_PHONE as string
 
-export const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY as string
-export const AWS_SECRET_KEY = process.env.AWS_SECRET_KEY as string
-export const AWS_REGION = process.env.AWS_REGION as string
+export const AWS_ACCESS_KEY = process.env.FX_AWS_ACCESS_KEY as string
+export const AWS_SECRET_KEY = process.env.FX_AWS_SECRET_KEY as string
+export const AWS_REGION = process.env.FX_AWS_REGION as string
 export const AWS_BUCKET_NAME = 'transfersharp'
