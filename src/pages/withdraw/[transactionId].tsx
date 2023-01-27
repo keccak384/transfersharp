7698ade571c166908d15ed88781b6185c2c1f350
Vignesh Mohankumar <me@vig.xyz>
2023-01-27 08:58:51 -0500

    withdraw styles

--- src/pages/withdraw/[transactionId].tsx
+++ src/pages/withdraw/[transactionId].tsx
@@ -1,5 +1,10 @@
 // pages/users/[uid].js
+import * as Label from '@radix-ui/react-label'
+import dynamic from 'next/dynamic'
+import Image from 'next/image'
+import { styled } from 'stitches.config'
 
+import { Button, FlexRowFixed, PageWrapper, SmallText } from '@/components/primitives'
 import { getTransactionById, Transaction } from '@/db/transactions'
 
 export async function getServerSideProps({ params: { transactionId } }: { params: { transactionId: string } }) {
@@ -14,14 +19,14 @@ export async function getServerSideProps({ params: { transactionId } }: { params
     }
   }
 
-  if (!transaction.hash) {
-    return {
-      redirect: {
-        destination: `/receive/${transaction.id}`,
-        permanent: false,
-      },
-    }
-  }
+  // if (!transaction.hash) {
+  //   return {
+  //     redirect: {
+  //       destination: `/receive/${transaction.id}`,
+  //       permanent: false,
+  //     },
+  //   }
+  // }
 
   return {
     props: {
@@ -30,11 +35,59 @@ export async function getServerSideProps({ params: { transactionId } }: { params
   }
 }
 
-export default function Withdraw({ transaction }: { transaction: Transaction }) {
+const ReceiveWrapper = styled('div', {
+  maxWidth: '500px',
+  padding: '24px',
+  border: '1px solid $gray6',
+  borderRadius: '24px',
+})
+
+const BigText = styled('span', {
+  fontSize: '42px',
+  appearance: 'none',
+  color: '$gray12',
+})
+
+const MediumText = styled('span', {
+  fontSize: '20px',
+  appearance: 'none',
+  color: '$gray12',
+})
+
+const FlexColumn = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '24px',
+})
+
+function Withdraw({ transaction }: { transaction: Transaction }) {
   return (
-    <div style={{ padding: 40 }}>
-      <h2>You received money from {transaction.fromPhoneNumber}</h2>
-      <p>Click here to withdraw</p>
-    </div>
+    <PageWrapper>
+      <ReceiveWrapper>
+        <FlexColumn>
+          <Label.Root htmlFor="youReceived">
+            <MediumText>You just received</MediumText>
+          </Label.Root>
+          <BigText>€920</BigText>
+          <FlexRowFixed>
+            <Image src="/EUR.png" alt="13" width={36} height={36} priority />
+            <BigText>EURO</BigText>
+          </FlexRowFixed>
+          <Button
+            as="a"
+            href="#"
+            onClick={() => {
+              console.log('here')
+            }}
+          >
+            Withdraw
+          </Button>
+        </FlexColumn>
+      </ReceiveWrapper>
+    </PageWrapper>
   )
 }
+
+export default dynamic(() => Promise.resolve(Withdraw), {
+  ssr: false,
+})
